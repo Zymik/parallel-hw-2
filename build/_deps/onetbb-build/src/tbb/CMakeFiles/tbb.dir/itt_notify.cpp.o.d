b8bcac6ea6d48e23a57cd2703cff3e141fef618e
Kosolapov Ivan <ivankosolapov@yandex-team.ru>
2024-01-17 07:10:11 +0300

    removed idea

--- build/_deps/onetbb-build/src/tbb/CMakeFiles/tbb.dir/itt_notify.cpp.o.d
+++
@@ -1,2 +0,0 @@
-_deps/onetbb-build/src/tbb/CMakeFiles/tbb.dir/itt_notify.cpp.o: \
-  /Users/ivankosolapov/CLionProjects/parallel_algo/build/_deps/onetbb-src/src/tbb/itt_notify.cpp
--- build/_deps/onetbb-build/src/tbbmalloc/CMakeFiles/tbbmalloc.dir/__/tbb/itt_notify.cpp.o.d
+++
@@ -1,2 +0,0 @@
-_deps/onetbb-build/src/tbbmalloc/CMakeFiles/tbbmalloc.dir/__/tbb/itt_notify.cpp.o: \
-  /Users/ivankosolapov/CLionProjects/parallel_algo/build/_deps/onetbb-src/src/tbb/itt_notify.cpp
